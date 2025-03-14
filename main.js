12f549eb705a517ae4f9f29508b9b71ddc6ad649
chakirben <72473487+chakirben@users.noreply.github.com>
2025-03-14 01:54:39 +0000

    checkin auth

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 import login from "./Login.js"
 import home from "./Home.js"
 let token = localStorage.getItem("jwt")
+
+if (token) {
+    let resp = await fetch("https://learn.zone01oujda.ma/api/graphql-engine/v1/graphql", {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+            query: `
+                {
+                    user {
+                        lastName
+                    }
+                }
+            `})
+    })
+    let data = await resp.json()
+    if (data.errors) {
+        login()
+    }
+    console.log(resp.ok);
+}
+
 if (!token) {
     console.log("not loggeds");
     login()
